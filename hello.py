a5aa5d90d6e0305c7c3cdc92ec269c954c545c6a
wenderson <ds.wendersonmelo@gmail.com>
2023-07-31 21:36:17 -0300

    Mudança

--- hello.py
+++ hello.py
@@ -33,19 +33,19 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
     
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)    
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
-        return '<Role %r>' % self.username
+        return '<User %r>' % self.username
     
 
 class NameForm(FlaskForm):
