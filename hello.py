3c0b6c4aa45ab032df430a20c5f02cd6bc8d4d15
wenderson <ds.wendersonmelo@gmail.com>
2023-08-03 19:48:33 -0300

    database use in View Functions

--- hello.py
+++ hello.py
@@ -56,19 +56,22 @@ class NameForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        #Supõe que o banco já está criado atráves do flask shell
+        user = User.query.filter_by(username = form.name.data).first()
+
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name = name)
-
-
-
-
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known = session.get('known', False))        
+        
