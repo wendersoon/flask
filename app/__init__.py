3673db5310d1e8528f8a589fec8a6544a20fba7a
wenderson <ds.wendersonmelo@gmail.com>
2023-08-12 20:07:12 -0300

    ok

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,14 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,13 +22,15 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+
+    # attach routes and custom error pages here
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
-    # attach routes and custom error pages here
-    
-    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix ='/auth')
 
     return app
 
