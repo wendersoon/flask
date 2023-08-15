626664284318074a4a71a1bd8753e48ed9ca6334
wenderson <ds.wendersonmelo@gmail.com>
2023-08-14 23:37:49 -0300

    Conexão com banco não está funcionando

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from . import auth
 from ..models import User
 from .forms import LoginForm
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -16,7 +16,7 @@ def login():
                 next = url_for(next)
             return redirect(next)
         flash('Invalid username or password.')
-    return render_template('auth/login.html', form)
+    return render_template('auth/login.html', form=form)
 
 
 @auth.route('/logout')
