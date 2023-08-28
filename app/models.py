629c21541846a08c695e2201e07efc67b4851d41
wenderson <ds.wendersonmelo@gmail.com>
2023-08-28 18:11:00 -0300

    comecei o cap 9

--- app/models.py
+++ app/models.py
@@ -5,14 +5,42 @@ from flask_login import UserMixin
 from . import login_manager
 from flask import current_app
 
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
+
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+    def __repr__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+    
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+            
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+            
+    def reset_permission(self):
+        self.permissions = 0
+        
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
+    
     
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -99,4 +127,6 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
 
