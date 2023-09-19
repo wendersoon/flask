4eb35f55af4535c49680c186a89fd74da568b86e
wenderson <ds.wendersonmelo@gmail.com>
2023-09-19 17:47:32 -0300

    Adding a Pagination Widget

--- app/main/views.py
+++ app/main/views.py
@@ -19,13 +19,14 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
     page = request.args.get('page', 1, type=int)
     pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
-    page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
-    error_out=False)
+        page=page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts, pagination=pagination) 
+    return render_template('index.html', form=form, posts=posts,
+                           pagination=pagination)
+
 
 @main.route('/user/<username>')
 def user(username):
--- config.py
+++ config.py
@@ -13,7 +13,8 @@ class Config:
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_MAIL_SENDER = f'Flasky Admin <wender.melo1912@hotmail.com>'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    FLASKY_POSTS_PER_PAGE = 10
+    
     @staticmethod
     def init_app(app):
         pass
