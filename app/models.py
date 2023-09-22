3bb76dc9ff7e7004cc63650e47640a9d58683e92
wenderson <ds.wendersonmelo@gmail.com>
2023-09-22 18:40:22 -0300

    adicionando tasks de users

--- app/models.py
+++ app/models.py
@@ -212,6 +212,29 @@ class User(UserMixin, db.Model):
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)    
     
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
+
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self, user):
+        if user.id is None:
+            return False
+        return self.followed.filter_by(
+            followed_id=user.id).first() is not None
+
+    def is_followed_by(self, user):
+        if user.id is None:
+            return False
+        return self.followers.filter_by(
+            follower_id=user.id).first() is not None
+        
+    
     def __repr__(self):
         return '<User %r>' % self.username
     
