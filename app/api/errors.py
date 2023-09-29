06038f43e215c386671b7e77f456e50ff63dfad6
wenderson <ds.wendersonmelo@gmail.com>
2023-09-29 16:59:11 -0300

    api 2

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,12 +1,8 @@
 from flask import render_template, request, jsonify
 
 
-@main.app_errorhandler(404)
-def page_not_found(e):
-    if request.accept_mimetypes.accept_json and \
-        not request.accept_mimetypes.accept_html:
-        response = jsonify({'error': 'not found'})
-        response.status_code = 404
-        return response
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
+    response.status_code = 403
+    return response
 
-    return render_template('404.html'), 404
