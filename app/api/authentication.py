b8566495f4c65f07eab64db9ef9127b43be45ee4
wenderson <ds.wendersonmelo@gmail.com>
2023-09-30 19:26:54 -0300

    finally part 2

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -2,7 +2,7 @@ from flask import g, jsonify
 from flask_httpauth import HTTPBasicAuth
 from ..models import User
 from . import api
-from .errors import unauthorized, forbidden
+from errors import forbidden
 
 auth = HTTPBasicAuth()
 
