479bd02852718d6c91fb4423a17703d328b7cbde
wenderson <ds.wendersonmelo@gmail.com>
2023-10-06 18:13:07 -0300

    chapter 16 complete

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,17 @@ from .. import db
 from ..models import User, Permission, Post, Role, Comment
 from ..email import send_email
 from ..decorators import admin_required, permission_required
+from flask_sqlalchemy import get_debug_queries
 
+@main.after_app_request
+def after_request(response):
+    for query in get_debug_queries():
+        if query.duration >- current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+            current_app.logger.warning(
+                'Slow query: %s\nParamenters: %s\nDuration: %f\nContext: %s\n'%
+                (query.statement, query.parameters, query.duration, query.context))
+        
+    return response
 
 @main.route('/shutdown')
 def server_shutdown():
--- config.py
+++ config.py
@@ -17,6 +17,8 @@ class Config:
     FLASKY_FOLLOWERS_PER_PAGE = 10
     FLASKY_COMMENTS_PER_PAGE = 25
     WTF_CSRF_ENABLED = False
+    SQLALCHEMY_RECORD_QUERIES = True
+    FLASKY_SLOW_DB_QUERY_TIME = 0.5
     
     @staticmethod
     def init_app(app):
