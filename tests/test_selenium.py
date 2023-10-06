e6a8d70286c2684087f0b1420230cede03400ea7
wenderson <ds.wendersonmelo@gmail.com>
2023-10-06 12:22:51 -0300

    chapter 15 complete

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,6 +1,7 @@
-from app import create_app
+from app import create_app, db, fake
+from app.models import User, Role, Post, Comment
 from selenium import webdriver
-import unittest
+import unittest, threading, re
 
 class SeleniuemTestCase(unittest.TestCase):
     client = None
@@ -9,7 +10,7 @@ class SeleniuemTestCase(unittest.TestCase):
     def setUpClass(cls):
         #start chrome
         options = webdriver.ChromeOptions()
-        options.add_argument('headless')
+        #options.add_argument('headless')
         try:
             cls.client = webdriver.Chrome(chrome_options=options)
         except:
@@ -26,3 +27,65 @@ class SeleniuemTestCase(unittest.TestCase):
             import logging
             logger = logging.getLogger('werkzeug')
             logger.setLevel('ERROR')
+            
+            #create the database and populate with some fake data
+            db.create_all()
+            Role.insert_roles()
+            fake.users(10)
+            fake.posts(10)
+            
+            #add an administrator user
+            admin_role = Role.query.filter_by(permissions=0xff).first()
+            admin = User(email='john@example.com',
+                         username='john', password='cat',
+                         role=admin_role, confirmed=True)
+            db.session.add(admin)
+            db.session.commit()
+            
+            #start the Flask server in a thread
+            cls.server_thread = threading.Thread(
+                target=cls.app.run, kwargs={'debug': 'false',
+                                            'use_reloader': False,
+                                            'use_debugger': False})
+            cls.server_thread.start()
+            
+    @classmethod
+    def tearDownClass(cls):
+        if cls.client:
+            #stop the Flask server and the browser
+            cls.client.get('http://localhost:5000/shutdown')
+            cls.client.quit()
+            cls.server_thread.join()
+            
+            # destroy database
+            db.drop_all()
+            db.session.remove()
+            # remove application context
+            cls.app_context.pop()
+            
+    def setUp(self):
+        if not self.client:
+            self.skipTest('Web browser not available')
+    def tearDown(self):
+        pass
+    
+    def test_admin_home_page(self):
+        # navigate to home page
+        self.client.get('http://localhost:5000/')
+        self.assertTrue(re.search('Hello,\s+Stranger!',
+                                  self.client.page_source))
+        
+        # navigate to login page
+        self.client.find_element_by_link_text('Log In').click()
+        self.assertIn('<h1>Login</h1>', self.client.page_source)
+        
+        # log in
+        self.client.find_element_by_name('email').\
+            send_keys('john@example.com')
+        self.client.find_element_by_name('password').send_keys('cat')
+        self.client.find_element_by_name('submit').click()
+        self.assertTrue(re.search('Hello,\s+john!', self.client.page_source))
+        
+        # navigate to the user's profile page
+        self.client.find_element_by_link_text('Profile').click()
+        self.assertIn('<h1>john</h1>', self.client.page_source)
